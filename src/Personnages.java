d231cb2ce0e1fff5de7b2e7f50cd28d3f2e2ae3a
AmFadma <fadma.amiche@u-psud.fr>
2021-04-29 13:03:42 +0200

    Partie 1.3 / Commentaires de type "Wyatt descend du toit.".

--- src/Personnages.java
+++ src/Personnages.java
@@ -15,14 +15,35 @@ public class Personnages {
         if(userInput.hasNextLine()){
             a = userInput.nextLine();
             Direction dir = Direction.valueOf(a);
-            if( dir== Direction.AVANT && pos_wag != 0){
-                pos_wag -=1;
-            }else if(dir== Direction.HAUT){
-                pos_toît = true ;
-            }else if(dir== Direction.ARRIERE && pos_wag != 4){
-                pos_wag +=1;
+            if( dir== Direction.AVANT ){
+                if( pos_wag == 0){
+                    System.out.println("Wyatt est deja dans le premier wagon.");
+                } else {
+                    pos_wag -= 1;
+                }
+            }else if(dir== Direction.HAUT ){
+                if (pos_toît == true){
+                    System.out.println("Wyatt est deja sur le toit.");
+                } else {
+
+                    pos_toît = true;
+                    System.out.println("Wyatt grimpe sur le toit.");
+
+                }
+            }else if(dir== Direction.ARRIERE ){
+                if(pos_wag == 4){
+                    System.out.println("Wyatt est deja dans le dernier wagon.");
+                } else {
+                    pos_wag += 1;
+
+                }
             }else if(dir== Direction.BAS){
-                pos_toît = false;
+                if (pos_toît == false){
+                    System.out.println("Wyatt est deja dans wagon.");
+                } else {
+                    pos_toît = false;
+                    System.out.println("Wyatt descend du toit.");
+                }
             }else{
                 System.out.println("pas d'actions possibles");
             }
