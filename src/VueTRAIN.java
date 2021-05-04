be22193a99aa175ded59b6494ede1cd305700f8d
AmFadma <fadma.amiche@u-psud.fr>
2021-05-04 17:13:39 +0200

    elements affichés en fonction de la taille de la fentere d'affichage

--- src/VueTRAIN.java
+++ src/VueTRAIN.java
@@ -5,14 +5,14 @@ import javax.swing.*;
 
 public class VueTRAIN extends JPanel implements Observer{
     private Train train;
-    private static final int taille = 50;
+    private static final int taille = 90;
 
    // public final static int  taille = 20;
     public VueTRAIN(Train train){
         this.train = train;
 
         this.train.addObserver(this);
-        Dimension dim = new Dimension(845, 445);
+        Dimension dim = new Dimension(8*taille+45, 4*taille+45);
         this.setPreferredSize(dim);
     }
     public void update(){
@@ -39,11 +39,11 @@ public class VueTRAIN extends JPanel implements Observer{
 
                 }
                 //g.fillRect(i*200,j*100,200,100);
-                g.fillRect(20+j*200,120+i*100,200,100);
+                g.fillRect(20+j*2*taille,120+i*taille,2*taille,1*taille);
                 g.setColor(Color.black);
 
 
-                g.drawRect(20+j*200,120+i*100,200,100);
+                g.drawRect(20+j*2*taille,120+i*taille,2*taille,1*taille);
 
 
                 //paint(g, modele.getCellule(i, j), (i - 1) * TAILLE, (j - 1) * TAILLE);
