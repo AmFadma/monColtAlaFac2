21b05938dcff25449ebd9c5759fcb4b410313a74
vykiiTor <vykiitor@gmail.com>
2021-05-04 15:37:18 +0200

    bon affichage du train et bourses ready

--- src/VueTRAIN.java
+++ src/VueTRAIN.java
@@ -4,13 +4,15 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class VueTRAIN extends JPanel implements Observer{
-    public Train train;
-    public final static int  taille = 12;
+    private Train train;
+    private static final int taille = 50;
+
+   // public final static int  taille = 20;
     public VueTRAIN(Train train){
         this.train = train;
 
         this.train.addObserver(this);
-        Dimension dim = new Dimension(taille * train.taille *10,taille * train.taille *10);
+        Dimension dim = new Dimension(845, 445);
         this.setPreferredSize(dim);
     }
     public void update(){
@@ -19,28 +21,29 @@ public class VueTRAIN extends JPanel implements Observer{
 
 
     public void paintComponent(Graphics g) {
-        super.repaint();
+        super.paintComponent(g);
 
-        for (int i = 0; i < this.train.getSize(); i++) {
+        for (int i = 0; i < 2; i++) {
             for (int j =0 ; j < this.train.taille; j++) {
                 /**
                  * ... Appeler une fonction d'affichage auxiliaire.
                  * On lui fournit les informations de dessin [g] et les
                  * coordonnées du coin en haut à gauche.
                  */
-                /*if(i == 1){
-                    g.setColor(Color.GRAY);
-                    g.drawRect(i*100,j*200,i*100,j*200);
+                if(i == 1){
+                    g.setColor(Color.LIGHT_GRAY);
+
                 }else{
-                    g.setColor(Color.pink);
-                    g.drawRect(i*100,j*200,100,200);
+                    g.setColor(Color.WHITE);
+
 
                 }
                 //g.fillRect(i*200,j*100,200,100);
+                g.fillRect(20+j*200,120+i*100,200,100);
                 g.setColor(Color.black);
 
-                 */
-                g.drawRect(i*100,j*200,100,200);
+
+                g.drawRect(20+j*200,120+i*100,200,100);
 
 
                 //paint(g, modele.getCellule(i, j), (i - 1) * TAILLE, (j - 1) * TAILLE);
