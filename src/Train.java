f57c512deab63e983d812f6e935d8612a543de81
vykiiTor <vykiitor@gmail.com>
2021-05-07 18:21:22 +0200

    Bouton en haut, bouton action pas fonctionnel et tableau d'actions chelou j'avais oublié train

--- src/Train.java
+++ src/Train.java
@@ -4,14 +4,14 @@ import java.util.Random;
 public class Train extends Observable{
     public final int taille;
     public ArrayList<ArrayList<Wagon>> train = new ArrayList<>();
-    public Personnages p;
+    public ArrayList<Personnages> personnages = new ArrayList<>();
+    public int nbTours = 3; //a mettre dans le main à l'avenir
 
-    public Train(int taille, Personnages  p){
+    public Train(int taille, String[] s){
         this.taille = taille;
-        this.p=p;
         for (int i = 0; i < 2; i++) {
             train.add(new ArrayList<>());
-            for (int j = 0; j< this.taille ; j++){
+            for (int j = 0; j < this.taille ; j++){
                 if(i == this.taille-1){
                     train.get(i).add(new Wagon(i,j,0,0,1));
                 }else{
@@ -20,6 +20,9 @@ public class Train extends Observable{
                 }
             }
         }//tab de wagon cf cours
+        for (String names : s){
+            personnages.add(new Personnages(names));
+        }
     }
     public int getSize(){
         return this.taille;
